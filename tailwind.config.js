badc48088cfe38c3ff9dc773e9bc8619d1552180
hexu <251323229@qq.com>
2020-06-15 11:13:27 +0800

    引入tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        'primary': '#409EFF',
+        'success': '#67C23A',
+        'warning': '#E6A23C',
+        'danger': '#F56C6C',
+        'info': '#909399'
+      },
+      textColor: {
+        'primary': '#303133',
+        'regular': '#606266',
+        'secondary': '#909399',
+        'placeholder': '#C0C4CC'
+      },
+      borderColor: {
+        'base': '#DCDFE6',
+        'light': '#E4E7ED',
+        'lighter': '#EBEEF5',
+        'lightest': '#F2F6FC'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+    preflight: false
+  }
+}
